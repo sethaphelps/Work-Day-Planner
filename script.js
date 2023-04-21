d1278b5f3cf20f07d34588728fa820f0b32381cf
sethaphelps <sethaphelps@gmail.com>
2023-04-20 17:45:07 -0700

    worked on local storage code

--- script.js
+++ script.js
@@ -22,12 +22,19 @@ function setHourColors() {
 
 function loadStoredData() {
   var eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
-  if (!eventsData) {
+  if (eventsData !== null) {
     eventsData = {
       hour8: "",
       hour9: "",
       hour10: "",
       hour11: "",
+      hour12: "",
+      hour13: "",
+      hour14: "",
+      hour15: "",
+      hour16: "",
+      hour17: "",
+      hour18: "",
     };
   }
   // todo load existing data from local storage
@@ -37,15 +44,23 @@ function handleSaveClick(event) {
   var hourBlock = $(event.target).parent();
   var value = hourBlock.children("textarea").val();
   var hour = hourBlock.attr("id").split("-")[1];
-
   eventsData["hour" + hour] = value;
 
   localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
 }
 
-$(".saveBtn").on("click", handleSaveClick);
-
 $(function () {
   loadStoredData();
   setHourColors();
 });
+
+$(".saveBtn").on("click", handleSaveClick);
+
+function generate(){
+  for (var i = 8; i <= 18; i++){
+    let textblockId = "hour-" +1
+    var textblock = $("hour" + i);
+    textblock.textContent = eventsData["hour" + i];
+  }
+}
+
